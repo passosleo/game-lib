f5a929fc85a6798bb36b0d3ac2f9f87b4dd4fd0f
passosleo <leonardopassos98@gmail.com>
2021-12-26 01:55:50 -0300

    feat: menu lateral

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,8 +3,17 @@ import React from 'react';
 import './styles.css';
 //Icons
 import Menu from '@material-ui/icons/Menu';
+import Store from '@material-ui/icons/Store';
+import Widgets from '@material-ui/icons/Widgets';
+import Star from '@material-ui/icons/Star';
+import Trophy from '@material-ui/icons/EmojiEvents'
+import Exit from '@material-ui/icons/ExitToApp'
+import Settings from '@material-ui/icons/Settings'
+import Help from '@material-ui/icons/Help'
 //Images
 import Avatar from '../../assets/images/avatar.jpg'
+//Navigation
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
@@ -18,38 +27,68 @@ const Home = () => {
             <div className="profile">
               <img src={Avatar} />
               <div className="flex">
-                <a href="#"><span>Leonardo Passos</span></a>
+                <a href="#"><span>Leonardo</span></a>
                 <div className="status"/>
               </div>
             </div>
             
             <div className="menuTitle">
-              <Menu className="icon"/>
+              <Menu />
               <h2>Menu</h2>
             </div>
             
-
             <nav>
               <ul>
                 <li>
-                  <button>Loja</button>
+                  <button className="btnMenu">
+                    <Store />
+                    <span>Loja</span>
+                  </button>
                 </li>
                 <li>
-                  <button>Biblioteca</button>
+                  <button className="btnMenu temp2">
+                    <Widgets />
+                    <span>Biblioteca</span>
+                  </button>
                 </li>
                 <li>
-                  <button>Favoritos</button>
+                  <button className="btnMenu">
+                    <Star />
+                    <span>Favoritos</span>
+                  </button>
                 </li>
                 <li>
-                  <button>Conquistas</button>
+                  <button className="btnMenu">
+                    <Trophy />
+                    <span>Conquistas</span>
+                  </button>
                 </li>
               </ul>
             </nav>
 
+            <div className="divisor" />
+
+            <div>
+              <ul className="menuFooter">
+                <li>
+                  <Link className="link" to="/">
+                    <Exit className="icon"/>
+                  </Link>
+                </li>
+                <li>
+                  <Help className="icon"/>
+                </li>
+                <li>
+                  <Settings className="icon"/>
+                </li>
+              </ul>
+            </div>
+
           </div>
 
           <div className="containerPrincipal">
-            <h1>home</h1>
+            <h1>Home</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing</p>
           </div>
 
         </div>
---
+++ src/utils/data.js
@@ -0,0 +1,32 @@
+export const DATA = [
+  {
+    id: 1,
+    name: 'Grand Theft Auto V',
+    image: '',
+    value: '49,90',
+  },
+  {
+    id: 2,
+    name: 'Red Dead Redemption 2',
+    image: '',
+    value: '109,90',
+  },
+  {
+    id: 3,
+    name: 'Battlefield V',
+    image: '',
+    value: '89,90',
+  },
+  {
+    id: 4,
+    name: 'Grand Theft Auto San Andreas',
+    image: '',
+    value: '19,90',
+  },
+  {
+    id: 5,
+    name: 'Counter Strike Global Offensive',
+    image: '',
+    value: 'Free',
+  },
+]
